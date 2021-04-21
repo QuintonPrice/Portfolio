e4e8c76ea12048b22783e7f9735bd58bec2c75eb
Quinton Price <60953984+QuintonPrice@users.noreply.github.com>
2021-04-21 16:35:11 -0700

    About me page formatting

--- portfolio-app/src/pages/About/About.js
+++ portfolio-app/src/pages/About/About.js
@@ -15,8 +15,10 @@ class About extends Component {
             <div id="about-div">
                 <Navbar navId="navb" logoId="logo" logoText="qprice" logoClass="mr-auto" class="navbar bg-light navbar-expand-md sticky-top navbar-fixed-top shadow ml-auto"/>
                 <h1 id="header" className="font-weight-bold text-uppercase text-center ">About Me</h1>
-                <div className="blog container rounded border shadow">
+                <div id="blog" className="container rounded border shadow">
                     <img id="portrait" className="rounded" alt="picture of author" src={aboutpic}></img>
+                    <h1 className="about-text font-weight-bold">Hi,</h1>
+                    <p className="about-text">My name is Quinton Price. I'm currently a sophomore Computer Science student at Walla Walla University.</p>
                 </div>
             </div>
         )
