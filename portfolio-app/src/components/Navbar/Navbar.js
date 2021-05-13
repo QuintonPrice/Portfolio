f592e26767abdbb8b8d81024dcedab91992c5328
Quinton Price <60953984+QuintonPrice@users.noreply.github.com>
2021-05-13 16:29:08 -0700

    navbar hamburger menu
    on mobile, the navbar displays a hamburger menu instead of all of the items. still need to figure out how to make items drop down

--- portfolio-app/src/components/Navbar/Navbar.js
+++ portfolio-app/src/components/Navbar/Navbar.js
@@ -10,14 +10,18 @@ class NavBar extends Component {
         return (
             <nav id={this.props.navId} className={this.props.class}>
                 <li id={this.props.logoId} className={this.props.logoClass}><p>{this.props.logoText}</p></li>
-                <ul id="links" className="navbar-nav ml-auto">
-                    <li className="textlink"><Link to="/home">Home</Link></li>
-                    <li className="textlink"><Link to="/about">About Me</Link></li>
-                    <li className="textlink"><Link to="/projects">Projects</Link></li>
-                    <li><a target="blank" href="https://github.com/QuintonPrice"><i className="fab fa-github"></i></a></li>
-                    <li><a target="blank" href="https://www.linkedin.com/in/quinton-price/"><i className="fab fa-linkedin"></i></a></li>
-                    <li><a href="javascript:"></a></li>
-                </ul>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"><i className="fas fa-bars"></i></span>
+                </button>
+                <div className="collapse navbar-collapse">
+                    <ul id="links" className="navbar-nav ml-auto">
+                        <li className="nav-item active textlink"><Link className="nav-link" to="/home">Home</Link></li>
+                        <li className="nav-item active textlink"><Link className="nav-link" to="/about">About Me</Link></li>
+                        <li className="nav-item active textlink"><Link className="nav-link" to="/projects">Projects</Link></li>
+                        <li className="nav-item active"><a className="nav-link" target="blank" href="https://github.com/QuintonPrice"><i className="fab fa-github"></i></a></li>
+                        <li className="nav-item active"><a className="nav-link" target="blank" href="https://www.linkedin.com/in/quinton-price/"><i className="fab fa-linkedin"></i></a></li>
+                    </ul>
+                </div>
             </nav >
         );
     }
