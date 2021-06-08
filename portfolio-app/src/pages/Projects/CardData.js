ad1fc27d44d52c66fa155e1bdaa85bd7c930a676
Quinton Price <60953984+QuintonPrice@users.noreply.github.com>
2021-06-08 15:18:33 -0700

    new project card

--- portfolio-app/src/pages/Projects/CardData.js
+++ portfolio-app/src/pages/Projects/CardData.js
@@ -6,16 +6,16 @@ export const CardData = [
         cardText: "One of the most recent projects, my own website! I've built this from scratch using ReactJS and deploy/host it using Netlify. Features include API calls, loading posts from json files, and more."
     },
     {
-        cardImg: "https://via.placeholder.com/1000x500/fafafa/?text=1000x500+Placeholder+Image",
-        cardURL: "#",
-        cardTitle: "Hiking Trail",
-        cardText: "App that uses a hiking trails API to compile a list of hiking trails with a search function!"    
+        cardImg: "https://www.incimages.com/uploaded_files/image/1920x1080/getty_583734066_335273.jpg",
+        cardURL: "https://github.com/QuintonPrice/CPTR405-Project",
+        cardTitle: "Android Travel Companion App",
+        cardText: "Android app I developed for a class that serves as a traveling tool. Gives user the ability to search for nearby locations with search criteria, look up information on a city or place, and view a map"    
     },
     {
         cardImg: "https://via.placeholder.com/1000x500/fafafa/?text=1000x500+Placeholder+Image",
         cardURL: "#",
-        cardTitle: "Card title",
-        cardText: "This is placeholder text. When a project is added, it will go here!"    
+        cardTitle: "Hiking Trail",
+        cardText: "App that uses a hiking trails API to compile a list of hiking trails with a search function!"    
     },
     {
         cardImg: "https://via.placeholder.com/1000x500/fafafa/?text=1000x500+Placeholder+Image",
