a91b04a7a71ea39f1607369694c74ef0c02454a5
Quinton Price <60953984+QuintonPrice@users.noreply.github.com>
2021-06-22 16:17:12 -0700

    contact form

--- portfolio-app/src/pages/About/About.js
+++ portfolio-app/src/pages/About/About.js
@@ -16,7 +16,7 @@ class About extends Component {
         //var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
 
         return (
-            <div id="about-div">
+            <div className="about-div">
                 <Navbar navId="navb" logoId="logo" logoText="qprice" logoClass="mr-auto" class="bg-white sticky-top navbar-fixed-top shadow ml-auto" />
                 <h1 id="header-about" className="font-weight-bold text-uppercase text-center ">About Me</h1>
                 <div id="blog" className="container rounded border shadow">
@@ -28,6 +28,26 @@ class About extends Component {
                             <p className="about-text" dangerouslySetInnerHTML={{ __html: item["content"] }}></p> // TODO: remove the map feature
                         )
                     })}
+                </div>
+
+                <div id="contact" className="container rounded border shadow">
+                    <h2>Contact me!</h2>
+                    <form name="contact-form" method="POST" data-netlify="true">
+                        <input type="hidden" name="form-name" value="contact-form"/>
+                        <div className="form-group font-weight-bold">
+                            <label for="nameInput">Name</label>
+                            <input type="text" name="name" class="form-control" id="nameInput" placeholder="Enter your name"></input>
+                        </div>
+                        <div className="form-group font-weight-bold">
+                            <label for="emailInput">Email Address</label>
+                            <input type="email" name="email" className="form-control" id="emailInput" placeholder="Enter email address"></input>
+                        </div>
+                        <div className="form-group font-weight-bold">
+                            <label for="textInput">Message</label>
+                            <textarea name="textarea" className="form-control" id="textInput" rows="4" placeholder="Enter your message"></textarea>
+                        </div>
+                        <button id="submit-button" type="submit" className="btn btn-primary">Submit</button>
+                    </form>
                 </div>
                 <a id="download-resume" href={resume} className="btn rounded border shadow" download="Quinton Price Resume 6-22-21"> Download My Resume </a>
             </div>
