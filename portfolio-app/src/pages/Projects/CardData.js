ac7f373651621514398f244482f1a8e7ca64c5af
Quinton Price <60953984+QuintonPrice@users.noreply.github.com>
2021-10-12 14:17:23 -0700

    Update CardData.js

--- portfolio-app/src/pages/Projects/CardData.js
+++ portfolio-app/src/pages/Projects/CardData.js
@@ -1,6 +1,6 @@
 export const CardData = [
     {
-        cardImg: "https://i.imgur.com/eqDtcm0.png",
+        cardImg: "https://blog.corp-site.envato.com/cdn-cgi/image/width=1200,quality=85,format=auto/uploads/2020/06/SOC101_Portfolio_Guide.png",
         cardURL: "https://github.com/QuintonPrice/Portfolio",
         cardTitle: "Portfolio Project",
         cardText: "One of the most recent projects, my own website! I've built this from scratch using ReactJS and deploy/host it using Netlify. Features include API calls, loading posts from json files, and more."
@@ -12,10 +12,10 @@ export const CardData = [
         cardText: "Android app I developed for a class that serves as a traveling tool. Gives user the ability to search for nearby locations with search criteria, look up information on a city or place, and view a map"    
     },
     {
-        cardImg: "https://via.placeholder.com/1000x500/fafafa/?text=1000x500+Placeholder+Image",
-        cardURL: "#",
-        cardTitle: "Hiking Trail",
-        cardText: "App that uses a hiking trails API to compile a list of hiking trails with a search function!"    
+        cardImg: "https://media.cntraveler.com/photos/60480c67ff9cba52f2a91899/16:9/w_2991,h_1682,c_limit/01-velo-header-seattle-needle.jpg",
+        cardURL: "https://github.com/QuintonPrice/project-CPTR220",
+        cardTitle: "Seattle Blog Project",
+        cardText: "Website that serves as a guide for Seattle. Developed with a group for a project in an Intro to Web Development class. Built with React and feature API requests, React state, and more."    
     },
     {
         cardImg: "https://via.placeholder.com/1000x500/fafafa/?text=1000x500+Placeholder+Image",
