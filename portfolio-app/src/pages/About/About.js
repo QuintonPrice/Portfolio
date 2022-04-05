5fbb21f2759f540a84e59a99cc8cac7fef652338
Quinton Price <60953984+QuintonPrice@users.noreply.github.com>
2022-04-05 15:48:44 -0700

    updated images

--- portfolio-app/src/pages/About/About.js
+++ portfolio-app/src/pages/About/About.js
@@ -27,8 +27,7 @@ class About extends Component {
                 <h1 id="header-about" className="font-weight-bold text-uppercase text-center ">About Me</h1>
                 <div id="blog" className="container rounded border shadow">
                     <figure>
-                        <img id="backpacking" className="rounded border shadow" alt="author" src={backpacking}></img>
-                        <figcaption className="font-italic">Backpacking in the Goat Rocks Wilderness</figcaption>
+                        <img id="backpacking" className="rounded border shadow" alt="author" src="https://lh3.googleusercontent.com/K1NQBYGk_-PsI85mfqLUkD4EsK0UnltOTvUblE76y8I3jtqxkmjzKukRvYS-65XDsuC354oE2uo4mBWxKqAAYGQD0yy7vgUjyMfWk74iVkhMrmsX-ZeoAy_FXc0nJ9IQF2jL5NJ2M7nEKqRSuQANujcvu21RqlQggVsrCvlwFXnSBWZ2zfj8c8lOi6Pj9DnCneL8-EmpVay8JIFE25czVapgQYjspNcHo4YQvcsl-8ENqEIUV-9ZfEsjotyYkt0qWPRR7Mz7zOAT8BXuygV26gdgyToFcUWrm7gu8BTpLsSwqeHmdCFwcGRioI4Mv0G880dZvQ3giCgQJ6xPUddgvZxhb1bC5TqDIKw1L1nQxEn-Tw7sU2iagwQdbTzDsbddoBmpPvtmHrM1gR_kt_TqNP3A2j9ra577bLhXCgc-lThkQO5xweo7XojYrMl8SN3KCRQh0Fauc2Es9vnRKR7vy8lREr-nqwrjwCZlxLz9UefHoxuBaaspx8s6zj3B6A18TF5NWCkqrjBxkcuDBkRBNYXGo3IY7aIujcksG6sgEBfJP9O3r1v1CwmGZxLFdejiOArxn938-JMYrEPRWUmqbmpzZ3wlKesLv3CzwqADc4y2uWQlkrFFFMngpW0VU4q6SDRYbw-EkOm1LTBomNLaoCYs_1P9aimPSHWVO72TbnoSImDTzIGj52mGYBMHQRlJJPgK53GoCMIgnjCRx8csUd8JGWwBsTB08R7taEDIGfK-UigT617V6u9TOR-_w5aaAxAIWaquk6ZkTrKiocfOq0s5Fp4asuAgl64Fol3hBEcccc5GjMJRcgUmJaKyVdgldWr5bLxs17nqx2NMRCn_17DtCRbnYaMtELlqslC8vDTk5PxwmuHbRm2LzwfPnQr5ve2giU5BmM6suG8ubE7hQs35p3BQ9BMW9XQdRdn55Z8sut60=w1024-h1365-no?authuser=0"></img>
                     </figure>
                     <h1 id="about-hi" className="about-text font-weight-bold">Hi,</h1>
                     <br />
--- portfolio-app/src/pages/Projects/CardData.js
+++ portfolio-app/src/pages/Projects/CardData.js
@@ -1,18 +1,18 @@
 export const CardData = [
     {
-        cardImg: "https://blog.corp-site.envato.com/cdn-cgi/image/width=1200,quality=85,format=auto/uploads/2020/06/SOC101_Portfolio_Guide.png",
+        cardImg: "https://images.pexels.com/photos/5554655/pexels-photo-5554655.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
         cardURL: "https://github.com/QuintonPrice/Portfolio",
         cardTitle: "Portfolio Project",
         cardText: "One of the most recent projects, my own website! I've built this from scratch using ReactJS and deploy/host it using Netlify. Features include API calls, loading posts from json files, and more."
     },
     {
-        cardImg: "https://www.incimages.com/uploaded_files/image/1920x1080/getty_583734066_335273.jpg",
+        cardImg: "https://images.pexels.com/photos/2007401/pexels-photo-2007401.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
         cardURL: "https://github.com/QuintonPrice/CPTR405-Project",
         cardTitle: "Android Travel Companion App",
         cardText: "Android app I developed for a class that serves as a traveling tool. Gives user the ability to search for nearby locations with search criteria, look up information on a city or place, and view a map"    
     },
     {
-        cardImg: "https://images.pexels.com/photos/7787422/pexels-photo-7787422.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+        cardImg: "https://images.pexels.com/photos/6465964/pexels-photo-6465964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
         cardURL: "https://github.com/QuintonPrice/TrailApp",
         cardTitle: "Hiking Trail App (In Progress)",
         cardText: "Website that will serve as a repository for hiking, mountain biking, and similar trails. Users will have the ability to register for accounts, add trails and pictures, save favorites, and more. Built using React.js, Google Firebase, Bootstrap, Google Authentication, and more."    
