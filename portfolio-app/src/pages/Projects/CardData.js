e309da0645eecbc9ebea5d84be33563218da9b89
Quinton Price <60953984+QuintonPrice@users.noreply.github.com>
2022-05-26 11:18:18 -0700

    updated pictures on projects page

--- portfolio-app/src/pages/Projects/CardData.js
+++ portfolio-app/src/pages/Projects/CardData.js
@@ -24,13 +24,13 @@ export const CardData = [
         cardText: "Website that serves as a guide for Seattle. Developed with a group for a project in an Intro to Web Development class. Built with React and feature API requests, React state, and more."    
     },
     {
-        cardImg: "https://images.pexels.com/photos/65169/pexels-photo-65169.jpeg",
+        cardImg: "https://images.pexels.com/photos/5477774/pexels-photo-5477774.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
         cardURL: "https://github.com/QuintonPrice/CPTR355-Project",
         cardTitle: "BabylonJS Checkers Game",
         cardText: "Created for a computer graphics class, this is a 3D checkers game created with BabylonJS! It features shadows, animations, blender meshes, and more. (Note: Picture is not accurate!)"
     },
     {
-        cardImg: "https://images.pexels.com/photos/3144205/pexels-photo-3144205.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        cardImg: "https://images.pexels.com/photos/1154510/pexels-photo-1154510.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
         cardURL: "https://github.com/QuintonPrice/Swift-Weather",
         cardTitle: "iOS Weather App (In Progress)",
         cardText: "Close clone of the default iOS weather app. Using this to learn SwitfUI basics."
