3535c784ff558c9a382015f349388ce71ac8aa94
Quinton Price <60953984+QuintonPrice@users.noreply.github.com>
2023-04-16 13:13:16 -0700

    Update CardData.js

--- portfolio-app/src/pages/Projects/CardData.js
+++ portfolio-app/src/pages/Projects/CardData.js
@@ -6,16 +6,16 @@ export const CardData = [
         cardText: "One of the most recent projects, my own website! I've built this from scratch using ReactJS and deploy/host it using Netlify. Features include API calls, loading posts from json files, and more."
     },
     {
-        cardImg: "https://images.pexels.com/photos/2007401/pexels-photo-2007401.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-        cardURL: "https://github.com/QuintonPrice/CPTR405-Project",
-        cardTitle: "Android Travel Companion App",
-        cardText: "Android app I developed for a class that serves as a traveling tool. Gives user the ability to search for nearby locations with search criteria, look up information on a city or place, and view a map"    
-    },
-    {
         cardImg: "https://images.pexels.com/photos/6465964/pexels-photo-6465964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
         cardURL: "https://github.com/QuintonPrice/TrailApp",
         cardTitle: "Hiking Trail App",
         cardText: "Website that serves as a repository for hiking, mountain biking, and similar trails. Users have the ability to register for accounts, add trails and pictures, and more. Built using React.js, Google Firebase, Bootstrap, Google Authentication, and more."    
+    },
+    {
+        cardImg: "https://images.pexels.com/photos/2007401/pexels-photo-2007401.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        cardURL: "https://github.com/QuintonPrice/CPTR405-Project",
+        cardTitle: "Android Travel Companion App",
+        cardText: "Android app I developed for a class that serves as a traveling tool. Gives user the ability to search for nearby locations with search criteria, look up information on a city or place, and view a map"    
     },
     {
         cardImg: "https://media.cntraveler.com/photos/60480c67ff9cba52f2a91899/16:9/w_2991,h_1682,c_limit/01-velo-header-seattle-needle.jpg",
